ba6282bb70512f06ebf94246d4589330b45af5c2
Monorflair <elroi.macharm@gmail.com>
2024-10-16 21:32:13 +0200

    many added places

---
+++ app.py
@@ -0,0 +1,17 @@
+import requests
+
+api_key = '3f88d24bae8b434e6bbdf0e28b846f94'
+
+user_input = input("Enter city: ")
+
+weather_data = requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
+
+if weather_data.json()['cod'] == '404':
+    print("No City Found")
+else:
+    weather = weather_data.json()['weather'][0]['main']
+    temp = round(weather_data.json()['main']['temp'])
+
+    print(f"The weather in {user_input} is: {weather}")
+    print(f"The temperature in {user_input} is: {temp}ºF")
